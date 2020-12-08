31bcd90d82e1bc921b91322bb4140e41dd5bee34
Douglas Mezuraro <douglasmez@gmail.com>
2020-12-08 15:13:39 -0300

    [Missão 10] Ajustes

--- territory/delphi/knight/mission-10/Mission10.dpr
+++ territory/delphi/knight/mission-10/Mission10.dpr
@@ -1,9 +1,22 @@
 program Mission10;
 
+//       Author : Douglas Mezuraro
+// Modification : 08/12/2020
+//        Level : Knight
+//      Mission : 10
+//    Objective : Em uma aplicação VCL, faça a importação do WSDL do site RegExLib
+//                http://regexlib.com/WebServices.asmx?WSDL. Este WebService contém um catálogo
+//                de expressões regulares para serem utilizadas. Faça um acesso ao WebService e
+//                consulte a expressão regular nº 3122, referente a validação de e-mails. Em
+//                seguida, utilize essa expressão para validar um endereço de e-mail informado pelo
+//                usuário, apresentando o texto "e-mail válido" ou "e-mail inválido" conforme o
+//                resultado.
+
 uses
   Vcl.Forms,
-  Form.Main in 'Form.Main.pas' {Main},
-  WebServices in 'WebServices.pas';
+  System.SysUtils,
+  WebService.RegExLib in 'src\WebService\WebService.RegExLib.pas',
+  Form.Main in 'src\Form\Form.Main.pas' {Main};
 
 {$R *.res}
 
@@ -14,6 +27,7 @@ begin
   Application.Run;
 
 {$WARN SYMBOL_PLATFORM OFF}
-  ReportMemoryLeaksOnShutdown := WordBool(DebugHook);
+  ReportMemoryLeaksOnShutdown := DebugHook.ToBoolean;
 {$WARN SYMBOL_PLATFORM DEFAULT}
 end.
+
---
+++ territory/delphi/knight/mission-10/src/Form/Form.Main.pas
@@ -0,0 +1,62 @@
+unit Form.Main;
+
+interface
+
+uses
+  System.Actions, System.Classes, System.RegularExpressions, System.UITypes, Vcl.ActnList,
+  Vcl.Controls, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.Forms, Vcl.StdCtrls, WebService.RegExLib;
+
+type
+  TMain = class sealed(TForm)
+  {$REGION 'Visual Components'}
+    EditEmail: TLabeledEdit;
+    ActionList: TActionList;
+    ActionValidate: TAction;
+    ButtonValidate: TButton;
+  {$ENDREGION}
+    procedure ActionValidateExecute(Sender: TObject);
+  strict private
+    FWebService: WebServicesSoap;
+  private
+    procedure Validate;
+  public
+    constructor Create(AOwner: TComponent); override;
+  end;
+
+var
+  Main: TMain;
+
+implementation
+
+{$R *.dfm}
+
+constructor TMain.Create(AOwner: TComponent);
+begin
+  inherited;
+  FWebService := WebService.RegExLib.GetWebServicesSoap(True);
+end;
+
+procedure TMain.ActionValidateExecute(Sender: TObject);
+begin
+  Validate;
+end;
+
+procedure TMain.Validate;
+const
+  EMAIL_PATTERN_ID: UInt16 = 3122;
+  VALIDATE_MESSAGE: array[Boolean] of string = ('E-mail inválido.', 'E-mail válido.');
+var
+  LResult: Boolean;
+  LEmailRegEx: RegExpDetails;
+begin
+  LEmailRegEx := FWebService.GetRegExpDetails(EMAIL_PATTERN_ID);
+  try
+    LResult := TRegEx.Create(LEmailRegEx.Regular_Expression).IsMatch(EditEmail.Text);
+    MessageDlg(VALIDATE_MESSAGE[LResult], TMsgDlgType.mtInformation, [TMsgDlgBtn.mbYes], 0);
+  finally
+    LEmailRegEx.Free;
+  end;
+end;
+
+end.
+
