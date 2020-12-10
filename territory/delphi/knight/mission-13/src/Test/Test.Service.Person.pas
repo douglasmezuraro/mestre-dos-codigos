b8cb397ad0188b86d6edfdb497bb70096a4dadf5
Douglas Mezuraro <douglasmez@gmail.com>
2020-12-10 10:40:23 -0300

    [Missão 13] Implementado casos de teste
    Quando os dados da pessoa estão completos

--- territory/delphi/knight/mission-13/src/Test/Test.Service.Person.pas
+++ territory/delphi/knight/mission-13/src/Test/Test.Service.Person.pas
@@ -11,30 +11,48 @@ type
   TServiceTest = class sealed
   strict private
     FService: IPessoaService;
-    FRepositoryMock: TMock<IPessoaRepository>;
+    FRepository: TMock<IPessoaRepository>;
   public
     [Setup]
     procedure Setup;
 
     [Test]
+    [TestCase('Valid person', 'Douglas Mezuraro,18/06/1996')]
+    procedure SavePersonWhenPersonIsValid(const AName, ABirthDate: string);
+
+    [Test]
     [TestCase('Empty name', ',18/06/1996,O nome não foi informado')]
     [TestCase('Empty birthdate', 'Douglas Mezuraro,,A Data de nascimento não foi informada')]
     [TestCase('Underage person', 'Douglas Mezuraro,10/12/2020,Menor de idade não pode ser cadastrado')]
-    procedure TestSavePersonWhenDataIsIncomplete(const AName, ABirthDate, AExceptionMessage: string);
+    procedure TestSavePersonWhenPersonIsInvalid(const AName, ABirthDate, AExceptionMessage: string);
   end;
 
 implementation
 
 procedure TServiceTest.Setup;
 begin
-  FRepositoryMock := TMock<IPessoaRepository>.Create;
-  FService := TPessoaService.Create(FRepositoryMock);
+  FRepository := TMock<IPessoaRepository>.Create;
+  FService := TPessoaService.Create(FRepository);
 end;
 
-procedure TServiceTest.TestSavePersonWhenDataIsIncomplete(const AName, ABirthDate, AExceptionMessage: string);
+procedure TServiceTest.SavePersonWhenPersonIsValid(const AName, ABirthDate: string);
+var
+  LPerson: TPessoa;
 begin
-  FRepositoryMock.Setup.WillRaise('PersistirDados', Exception);
+  FRepository.Setup.WillReturnDefault('PersistirDados', True);
+  LPerson := TPessoa.Create;
+  try
+    LPerson.Nome := AName;
+    LPerson.DataNascimento := System.SysUtils.StrToDateDef(ABirthDate, 0);
 
+    Assert.IsTrue(FService.Salvar(LPerson));
+  finally
+    LPerson.Free;
+  end;
+end;
+
+procedure TServiceTest.TestSavePersonWhenPersonIsInvalid(const AName, ABirthDate, AExceptionMessage: string);
+begin
   Assert.WillRaiseWithMessage(
     procedure
     var
