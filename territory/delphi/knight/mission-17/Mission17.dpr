d7175bc39677c01193fc52f4141c68c99359d219
Douglas Mezuraro <douglasmez@gmail.com>
2020-12-21 21:43:47 -0300

    [Missão 17] Incluído arquivos da missão 13

--- territory/delphi/knight/mission-17/Mission17.dpr
+++ territory/delphi/knight/mission-17/Mission17.dpr
@@ -15,13 +15,20 @@ program Mission17;
 
 uses
   System.SysUtils,
-  Vcl.Forms;
+  Vcl.Forms,
+  uPessoa in '..\mission-13\src\3rdParty\AppDesktop\uPessoa.pas',
+  uPessoaForm in '..\mission-13\src\3rdParty\AppDesktop\uPessoaForm.pas' {frmSalvadorDePessoas},
+  uPessoaRepository.Impl in '..\mission-13\src\3rdParty\AppDesktop\uPessoaRepository.Impl.pas',
+  uPessoaRepository in '..\mission-13\src\3rdParty\AppDesktop\uPessoaRepository.pas',
+  uPessoaService.Impl in '..\mission-13\src\3rdParty\AppDesktop\uPessoaService.Impl.pas',
+  uPessoaService in '..\mission-13\src\3rdParty\AppDesktop\uPessoaService.pas';
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TfrmSalvadorDePessoas, frmSalvadorDePessoas);
   Application.Run;
 
 {$WARN SYMBOL_PLATFORM OFF}
