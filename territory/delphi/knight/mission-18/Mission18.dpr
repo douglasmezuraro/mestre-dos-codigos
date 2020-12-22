02b4baf099166fedd9f77082db126e29ef7096a5
Douglas Mezuraro <douglasmez@gmail.com>
2020-12-21 23:24:05 -0300

    [Missão 18] Iniciado a missão

---
+++ territory/delphi/knight/mission-18/Mission18.dpr
@@ -0,0 +1,24 @@
+program Mission18;
+
+// Author: Douglas Mezuraro
+// Modification: 21/12/2020
+// Level: Knight
+// Mission: 18
+
+uses
+  System.SysUtils,
+  Vcl.Forms,
+  Form.Main in 'src\Form\Form.Main.pas' {Main};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TMain, Main);
+  Application.Run;
+
+{$WARN SYMBOL_PLATFORM OFF}
+  ReportMemoryLeaksOnShutdown := DebugHook.ToBoolean;
+{$WARN SYMBOL_PLATFORM DEFAULT}
+end.
---
+++ territory/delphi/knight/mission-18/src/Form/Form.Main.pas
@@ -0,0 +1,20 @@
+unit Form.Main;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+
+type
+  TMain = class sealed(TForm)
+  end;
+
+var
+  Main: TMain;
+
+implementation
+
+{$R *.dfm}
+
+end.
