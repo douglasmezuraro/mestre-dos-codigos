3183fffb56e53fd4d7610fadf40fcf20dc5c0fb9
Douglas Mezuraro <douglasmez@gmail.com>
2020-12-23 23:19:51 -0300

    [Missão 19] Iniciado a missão

---
+++ territory/delphi/knight/mission-19/Mission19.dpr
@@ -0,0 +1,25 @@
+program Mission19;
+
+// Author: Douglas Mezuraro
+// Modification: 23/12/2020
+// Level: Knight
+// Mission: 19
+
+uses
+  System.SysUtils,
+  Vcl.Forms,
+  Form.Main in 'src\Form\Form.Main.pas' {Main};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TMain, Main);
+  Application.Run;
+
+{$WARN SYMBOL_PLATFORM OFF}
+  ReportMemoryLeaksOnShutdown := DebugHook.ToBoolean;
+{$WARN SYMBOL_PLATFORM DEFAULT}
+end.
+
