40e940f380d1b22f46de73d024d2ce42d26ee7d6
Douglas Mezuraro <douglasmez@gmail.com>
2021-06-21 18:17:15 -0300

    [.NET][Squire][Mission 03] Finalizado missão 3

---
+++ territory/dotnet/squire/mission-03/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace mission_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Missão 03");
+
+            var number = 1;
+            while (number <= 100)
+            {
+                if (number % 3 == 0)
+                {
+                    Console.WriteLine($"O número {number} é multíplo de {3}.");
+                }
+                number++;
+            }
+        }
+    }
+}
