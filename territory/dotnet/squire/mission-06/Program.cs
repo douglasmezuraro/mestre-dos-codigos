9fda3137ef560ea99d622f197522745a283abf24
Douglas Mezuraro <douglasmez@gmail.com>
2021-06-23 14:58:11 -0300

    [.NET][Squire][Mission 06] Finalizado missão 6

---
+++ territory/dotnet/squire/mission-06/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace mission_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Missão 6");
+
+            // "x" não precisa ter valor atribuído
+            int x;
+            Out(out x);
+            Console.WriteLine($"x = {x}");
+
+            // "y" precisa ter valor atribuído
+            int y = 1023;
+            Ref(ref y);
+            Console.WriteLine($"y = {y}");
+        }
+
+        private static void Out(out int a)
+        {
+            // "a" precisa ter valor atribuído
+            a = 512;
+        }
+
+        private static void Ref(ref int b)
+        {
+            // "b" não precisa ter valor atribuído
+            b++;
+        }
+    }
+}
